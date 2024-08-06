f615e8e5014482c7776f57a65d3aaf3513b94393
benxta4424 <benxtaben@gmail.com>
2024-08-06 16:02:39 +0100

    Fixed a few issues that i kept running into in the previous linked_list
    Created the functions with less overload in order to keep more readable
    Added an insert method for adding values easier
    Soon i will add a delete_at_index method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,8 +1,161 @@
 class List
-    class Node
-        
+  class Node
+    attr_accessor :next_node, :value
+
+    def initialize(val)
+      @next_node = nil
+      @value = val
+    end
+  end
+
+  def initialize
+    @head = nil
+    @size = 0
+  end
+
+  def create_node(value)
+    Node.new(value)
+  end
+
+  def append_value(value)
+    new_node = create_node(value)
+
+    if @head.nil?
+      @head = new_node
+      @size += 1
+    else
+      current_node = @head
+
+      current_node = current_node.next_node while current_node.next_node
+      current_node.next_node = new_node
+      @size += 1
+    end
+  end
+
+  def prepend_values(value)
+    new_node = create_node(value)
+
+    new_node.next_node = @head
+    @head = new_node
+    @size += 1
+  end
+
+  def to_s
+    return "Lista goala!" if @head.nil?
+
+    current_node = @head
+    elements = []
+
+    while current_node
+      elements << "(#{current_node.value})"
+      current_node = current_node.next_node
+    end
+    elements.join("->")
+  end
+
+  def size
+    puts
+    puts "The size of the list is:#{@size}"
+  end
+
+  def head
+    puts
+    puts "The head of the list is:#{@head.value}"
+  end
+
+  def tail
+    current_node = @head
+    current_node = current_node.next_node while current_node.next_node
+    puts
+    puts "The tail of the list is:#{current_node.value}"
+  end
+
+  def at_index(index)
+    index2 = 0
+    current_node = @head
+
+    while current_node.next_node
+      current_node = current_node.next_node
+      index2 += 1
+
+      break if index == index2
     end
 
+    puts
+    puts "The value at index #{index} is:#{current_node.value}"
+  end
 
+  def pop_element
+    current_node = @head
+    previous_node = nil
+
+    nil if current_node.nil?
+    @head=nil if current_node.next_node.nil?
+    
+    while current_node.next_node
+      previous_node = current_node
+      current_node = current_node.next_node
+    end
+
+    previous_node.next_node = nil
+    @size-=1
+  end
+
+  def contains?(value)
+    current_node=@head
+
+    if current_node.next_node.nil? && current_node.value == value
+        return true
+    end
+
+    while current_node.next_node
+        if current_node.value == value
+            return true
+            break
+        end
+        current_node=current_node.next_node
+    end
+    
+    false
+  end
+
+  def find_value(val)
+    current_node=@head
+    index=0
+    index if current_node.next_node.nil? && current_node.value == val
+
+    while current_node.next_node
+        if current_node.value == val
+            return index
+            break
+        end
+        index+=1
+        current_node=current_node.next_node
+    end
+  end
+
+  def insert_at(val,index)
+    
+    new_node=create_node(val)
+    
+
+    if index==0
+        new_node.next_node = @head
+        @head=new_node
+    end
+
+    ind=0
+    current_node=@head
+
+    while current_node.next_node
+        ind+=1
+        break if ind==index
+        
+        current_node=current_node.next_node
+    end
+
+    new_node.next_node = current_node.next_node
+    current_node.next_node = new_node
+  end
 
 end
---
+++ main.rb
@@ -0,0 +1,51 @@
+require "./lib/linked_list"
+
+list = List.new
+
+list.append_value("sakina")
+list.append_value("lauren")
+list.append_value("jamie")
+list.append_value("jax")
+list.prepend_values("james")
+list.prepend_values("john")
+list.prepend_values("andrei")
+list.prepend_values("marcu")
+
+puts list
+
+list.size
+list.head
+list.tail
+list.at_index(6)
+list.pop_element
+
+puts
+puts "!! The list after pop !!"
+puts
+puts list
+puts 
+puts "!! The size after pop !!"
+list.size
+
+puts
+print "Ce valoare cauti:"
+contine=gets.chomp.to_s
+verifica=list.contains?(contine)
+verfica_2=nil
+if verifica==true
+    verifica2='este in lista'
+else
+    verifica2='nu este in lista'
+end
+puts
+puts "Valoarea '#{contine}' #{verifica2}"
+
+puts
+print "Value to find the index of:"
+value=gets.chomp.to_s
+puts 
+puts "'#{value}' is at index:#{list.find_value(value)}"
+
+list.insert_at(13,2)
+
+puts list
